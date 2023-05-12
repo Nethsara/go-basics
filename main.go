b69623b95298281c1efa4ae0a1e5918eddb8d4d8
Nethsara <nethsarasiyum2@gmail.com>
2023-05-13 03:40:40 +0530

    if conditions

--- main.go
+++ main.go
@@ -3,67 +3,91 @@ package main
 import (
 	"fmt"
 	"strings"
-) 
+)
 
-func main(){
-	conferenceName  := "Go Conferene"
+func main() {
+
 	const conferenceTickets int = 50
-	var remainingTickets  uint = 50
-	var bookings []string
-		
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v, We have available %v \n", conferenceTickets, remainingTickets)
-	fmt.Println("Here you can start booking")
+	var remainingTickets uint = 50
+	conferenceName := "Go Conference"
+	bookings := []string{}
 
-	for{
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
 
+	for {
 		var firstName string
 		var lastName string
 		var email string
 		var userTickets uint
-		
-		fmt.Print("What is your first name : ")
-		fmt.Scan(&firstName)
 
-		fmt.Print("What is your last name : ")
-		fmt.Scan(&lastName)
+		// asking for user input
+		fmt.Println("Enter Your First Name: ")
+		fmt.Scanln(&firstName)
 
-		fmt.Print("What is your email : ")
-		fmt.Scan(&email)
+		fmt.Println("Enter Your Last Name: ")
+		fmt.Scanln(&lastName)
 
-		fmt.Print("Enter number of tickets : ")
-		fmt.Scan(&userTickets)
-		
-		if userTickets > remainingTickets{
-			fmt.Printf("We only have %v tickets remaining.", remainingTickets)
+		fmt.Println("Enter Your Email: ")
+		fmt.Scanln(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scanln(&userTickets)
+
+		// validate user input
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			// book ticket in system
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			// print only first names
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names %v\n", firstNames)
+
+			// exit application if no tickets are left
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("firt name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
 			continue
 		}
-
-		remainingTickets -= userTickets
-		bookings = append(bookings, firstName+" " + lastName)
-
-		fmt.Printf("Whole slice : %v\n", bookings)
-		fmt.Printf("First value : %v\n", bookings[0])
-		fmt.Printf("Slice type : %T\n", bookings)
-		fmt.Printf("Length slice : %v\n", len(bookings))
-
-		fmt.Printf("%v tickets remaining\n", remainingTickets)
-
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName,lastName, userTickets, email)
-		
-		firstNames := []string{}
-		for _, booking := range bookings{
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		
-		fmt.Printf("First Names of the bookings %v\n", firstNames)
-
-		if remainingTickets == 0 {
-			fmt.Println("Our conference if already booked up")
-			break
-		}
-
 	}
 
+	// Switch statement example
+	city := "London"
+
+	switch city {
+	case "New York":
+		// booking for New York conference
+	case "Singapore", "Hong Kong":
+		// booking for Singapore & Hong Kong conference
+	case "London", "Berlin":
+		// booking for London & Berlin conference
+	case "Mexico City":
+		// booking for Mexico City conference
+	default:
+		fmt.Print("No valid city selected")
+	}
 }
