b1ee3acaf4eee05d70147a2c749cfaa5e5976647
Nethsara <nethsarasiyum2@gmail.com>
2023-05-13 02:13:00 +0530

    Pointers

--- main.go
+++ main.go
@@ -6,19 +6,28 @@ func main(){
 	conferenceName  := "Go Conferene"
 	const conferenceTickets int = 50
 	var remainingTickets  uint = 50
-	
-
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceTickets, remainingTickets)
-	
+		
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v, We have available %v \n", conferenceTickets, remainingTickets)
 	fmt.Println("Here you can start booking")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
+	fmt.Print("What is your first name : ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
+	fmt.Print("What is your last name : ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("What is your email : ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter number of tickets : ")
+	fmt.Scan(&userTickets)
+
 	userTickets = 2
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName,lastName, userTickets, email)
 
 }
