2853bceb7e07c8bff2b8e8335fc2c3c00bbc1fea
Nethsara <nethsarasiyum2@gmail.com>
2023-05-12 21:04:51 +0530

    variables and const

--- main.go
+++ main.go
@@ -3,6 +3,15 @@ package main
 import "fmt" 
 
 func main(){
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go Conferene"
+	const conferenceTickets = 50
+	var remainingTickets  = conferenceTickets
+
+	
+	fmt.Println("Welcome to",conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "We have available", remainingTickets)
 	fmt.Println("Here you can start booking")
+
+	
+
 }
