b7b26155e7c056e4e0dfd53f820c9386859eb913
Nethsara <nethsarasiyum2@gmail.com>
2023-05-12 21:19:28 +0530

    basic data structures

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import "fmt" 
 
 func main(){
-	var conferenceName = "Go Conferene"
-	const conferenceTickets = 50
-	var remainingTickets  = conferenceTickets
+	conferenceName  := "Go Conferene"
+	const conferenceTickets int = 50
+	var remainingTickets  uint = 50
+	
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceTickets, remainingTickets)
 	
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v We have available %v", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of %v, We have available %v \n", conferenceTickets, remainingTickets)
 	fmt.Println("Here you can start booking")
 
-	
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 
 }
